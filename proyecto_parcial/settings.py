5027fb94343ea11a7dea51d2fec955a71ddd0c70
admin <admin@admin.com>
2025-11-26 16:32:42 -0300

    true

--- proyecto_parcial/settings.py
+++ proyecto_parcial/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@(^%(v2g&a^h+2+s$7%n$fbv88od@j&@fiu(^3kjvd+aslz=^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['proyecto-parcial-elian.onrender.com', 'localhost', '127.0.0.1']
 
